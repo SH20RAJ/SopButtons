8b679a52aec38aff66ef5a46b78bc6ad2513be86
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-29 08:36:23 +0000

    ...

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var buttons = document.getElementById('buttonscon');
 var btnjson = WebScrapper.fetchjson('https://api.cssbuttons.io/buttons/').buttons;
-var btnhtml = '';
-for (let i = 0; i < btnjson.length; i++) {
-  btnhtml = `
-  
-  <div class="col-sm-6">
-  <div class="card p${btnjson[i].username}-${btnjson[i].friendlyId}" style="width: 18rem;">
-  
-  <div class="card-body">
-    ${btnjson[i].html}<style>${btnjson[i].scopedCss}</style>
-  </div>
-</div>
-</div>
+var limit = 20;
 
+for (let i = 0; i < limit; i++) {
+  let html = btnjson[i].html;
+  let cls = `p${btnjson[i].username}-${btnjson[i].friendlyId}`;
+  let css = btnjson[i].scopedCss;
+
+  let btnhtml = `
+   <div class="card ${cls}">
+   <center>
+      <div class="card-body text-center">
+      
+        <p class="card-text"></p>
+        <div class="playground">
+        ${html}
+        <style>${css}</style>
+        </div>
+        
+        
+      </div>
+      <div class="card-footer">
+      <a href="detail.html?id${i}" class="btn btn-primary">Get</a>
+      </div>
+    </center>
+    </div>
   `;
   buttons.insertAdjacentHTML('beforeend',btnhtml);
 }
